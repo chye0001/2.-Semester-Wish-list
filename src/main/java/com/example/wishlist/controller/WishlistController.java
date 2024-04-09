b1a382d84572c2a60d8fd0ab7208712e902e14d5
chye0001 <chye0001@stud.kea.dk>
2024-04-09 13:12:44 +0200

    added endpoint (/"create") for get- and postMapping for creating wishlist

--- src/main/java/com/example/wishlist/controller/WishlistController.java
+++ src/main/java/com/example/wishlist/controller/WishlistController.java
@@ -1,7 +1,30 @@
 package com.example.wishlist.controller;
 
+import com.example.wishlist.service.WishlistService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/wishlist")
 public class WishlistController {
+
+    private WishlistService wishlistService;
+
+    public WishlistController(WishlistService wishlistService){
+        this.wishlistService = wishlistService;
+    }
+
+    @GetMapping("/create")
+    public String createWishlist(){
+        return "wishlist-create";
+    }
+
+    @PostMapping("/create")
+    public String createWishlist(@RequestParam("title") String wishlistTitle, @RequestParam String pictureLink){
+        wishlistService.createWishlist(wishlistTitle, pictureLink);
+        return "redirect:/wishlist";
+    }
 }
