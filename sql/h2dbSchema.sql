0c16d1b2a8720838581dd3c071c444fe0a902a9f
chye0001 <chye0001@stud.kea.dk>
2024-04-10 13:06:41 +0200

    added hwdbSchema.sql and changed sql dialect to match h2db

---
+++ sql/h2dbSchema.sql
@@ -0,0 +1,37 @@
+CREATE SCHEMA IF NOT EXISTS wishlist_db;
+SET SCHEMA wishlist_db;
+
+CREATE TABLE users(
+                      username VARCHAR(50) NOT NULL PRIMARY KEY,
+                      password VARCHAR(50) NOT NULL,
+                      enabled BOOLEAN NOT NULL
+);
+
+CREATE TABLE authorities (
+                             username VARCHAR(50) NOT NULL,
+                             authority VARCHAR(50) NOT NULL,
+                             CONSTRAINT fk_authorities_users FOREIGN KEY(username) REFERENCES users(username)
+);
+
+
+CREATE TABLE wishlist(
+                         wishlist_id INT AUTO_INCREMENT,
+                         name VARCHAR(250) NOT NULL,
+                         picture VARCHAR(250),
+                         username VARCHAR(50),
+                         PRIMARY KEY (wishlist_id),
+                         FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE wish(
+                     wish_id INT AUTO_INCREMENT,
+                     name VARCHAR(250) NOT NULL,
+                     description VARCHAR(500),
+                     link VARCHAR(250),
+                     price DECIMAL(11,2),
+                     picture VARCHAR(250),
+                     reserved BOOLEAN DEFAULT FALSE,
+                     wishlist_id INT,
+                     PRIMARY KEY (wish_id),
+                     FOREIGN KEY (wishlist_id) REFERENCES wishlist(wishlist_id)
+);
