657283112d2837a35d057768239a3e50fb02a9b0
Lars <laha0006@stud.kea.dk>
2024-04-11 00:31:43 +0200

    added registerUser method

--- src/main/java/com/example/wishlist/service/UserService.java
+++ src/main/java/com/example/wishlist/service/UserService.java
@@ -1,7 +1,26 @@
 package com.example.wishlist.service;
 
+import com.example.wishlist.dto.UserDto;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private UserDetailsManager userDetailsManager;
+
+    public UserService(UserDetailsManager userDetailsManager) {
+        this.userDetailsManager = userDetailsManager;
+    }
+
+    public void registerUser(UserDto user) {
+        UserDetails userToRegister = User.builder()
+                .username(user.username())
+                .password(user.password())
+                .roles("USER").build();
+        userDetailsManager.createUser(userToRegister);
+    }
+
 }
