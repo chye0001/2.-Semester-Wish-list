e63f8f0c6d15a669f1a213e5c1caed27e6e72d24
Lars <laha0006@stud.kea.dk>
2024-04-11 00:04:02 +0200

    implemented basic filterchain bean, and UserDetailsManager bean

--- src/main/java/com/example/wishlist/config/SecurityConfig.java
+++ src/main/java/com/example/wishlist/config/SecurityConfig.java
@@ -1,9 +1,38 @@
 package com.example.wishlist.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/wishlists").hasRole("USER")
+                        .anyRequest().permitAll()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsManager users(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+
 }
