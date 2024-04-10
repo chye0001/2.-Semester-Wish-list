fcf6b35b9f91f22eca3dffad45182eaab92a1a3f
chye0001 <chye0001@stud.kea.dk>
2024-04-10 10:11:04 +0200

    added getWishes()

--- src/main/java/com/example/wishlist/repository/CRUDOperations.java
+++ src/main/java/com/example/wishlist/repository/CRUDOperations.java
@@ -1,6 +1,12 @@
 package com.example.wishlist.repository;
 
+import com.example.wishlist.model.Wish;
+
+import java.util.List;
+
 public interface CRUDOperations {
 
     boolean createWishlist(String wishlistTitle, String pictureLink);
+
+    List<Wish> getWishes (String wishlistName);
 }
