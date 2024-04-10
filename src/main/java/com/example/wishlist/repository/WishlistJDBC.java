346b6368570fbf6acbfb0b5c90fbb6e3b8320705
chye0001 <chye0001@stud.kea.dk>
2024-04-10 11:59:17 +0200

    added addWish(Wish newWish, String wishlistName)

--- src/main/java/com/example/wishlist/repository/WishlistJDBC.java
+++ src/main/java/com/example/wishlist/repository/WishlistJDBC.java
@@ -16,15 +16,15 @@ public class WishlistJDBC implements CRUDOperations {
 
     private DataSource dataSource;
 
-    public WishlistJDBC(DataSource dataSource){
+    public WishlistJDBC(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
     @Override
-    public boolean createWishlist(String wishlistTitle, String pictureLink){
+    public boolean createWishlist(String wishlistTitle, String pictureLink) {
         int affectedRows = 0;
-        
-        try (Connection connection = dataSource.getConnection()){
+
+        try (Connection connection = dataSource.getConnection()) {
             String createWishlist = "INSERT INTO wishlist (name, picture) VALUES (?, ?)";
             PreparedStatement pstmt = connection.prepareStatement(createWishlist);
             pstmt.setString(1, wishlistTitle);
@@ -32,7 +32,7 @@ public class WishlistJDBC implements CRUDOperations {
 
             affectedRows = pstmt.executeUpdate();
 
-        }catch (SQLException sqlException){
+        } catch (SQLException sqlException) {
             sqlException.printStackTrace();
         }
 
@@ -43,43 +43,73 @@ public class WishlistJDBC implements CRUDOperations {
     public List<Wish> getWishes(String wishlistName) {
         List<Wish> wishes = new ArrayList<>();
 
-        try (Connection connection = dataSource.getConnection()){
-        String getWishesOnWishlistName =
-                "SELECT " +
-                "w.name, " +
-                "w.description, " +
-                "w.link, " +
-                "w.price, " +
-                "w.picture, " +
-                "w.reserved " +
-                "FROM wishlist wl " +
-                "JOIN wish w " +
-                    "ON wl.wishlist_id = w.wishlist_id " +
-                "WHERE wl.name = ?";
+        try (Connection connection = dataSource.getConnection()) {
+            String getWishesOnWishlistName =
+                    "SELECT " +
+                            "w.name, " +
+                            "w.description, " +
+                            "w.link, " +
+                            "w.price, " +
+                            "w.picture, " +
+                            "w.reserved " +
+                            "FROM wishlist wl " +
+                            "JOIN wish w " +
+                            "ON wl.wishlist_id = w.wishlist_id " +
+                            "WHERE wl.name = ?";
 
-        PreparedStatement pstmt = connection.prepareStatement(getWishesOnWishlistName);
-        pstmt.setString(1, wishlistName);
+            PreparedStatement pstmt = connection.prepareStatement(getWishesOnWishlistName);
+            pstmt.setString(1, wishlistName);
 
-        ResultSet wishesResultSet = pstmt.executeQuery();
+            ResultSet wishesResultSet = pstmt.executeQuery();
 
-        while(wishesResultSet.next()) {
-            Wish newWish = new Wish(
-                    wishesResultSet.getString(1),
-                    wishesResultSet.getString(2),
-                    wishesResultSet.getDouble(4),
-                    wishesResultSet.getString(3),
-                    wishesResultSet.getString(5),
-                    wishesResultSet.getBoolean(6));
+            while (wishesResultSet.next()) {
+                Wish newWish = new Wish(
+                        wishesResultSet.getString(1),
+                        wishesResultSet.getString(2),
+                        wishesResultSet.getDouble(4),
+                        wishesResultSet.getString(3),
+                        wishesResultSet.getString(5),
+                        wishesResultSet.getBoolean(6));
 
-            wishes.add(newWish);
-        }
+                wishes.add(newWish);
+            }
 
 
-        }catch (SQLException sqlException) {
+        } catch (SQLException sqlException) {
             sqlException.printStackTrace();
         }
 
         return wishes;
     }
 
+    @Override
+    public boolean addWish(Wish newWish, String wishlistName) {
+        boolean isAdded = false;
+
+        try (Connection connection = dataSource.getConnection()){
+            String getWishlistIDOnWishlistName = "SELECT wishlist_id FROM wishlist WHERE name = ?";
+            PreparedStatement pstmtGetWishlistID = connection.prepareStatement(getWishlistIDOnWishlistName);
+            pstmtGetWishlistID.setString(1, wishlistName);
+            ResultSet wishlistIDResultSet = pstmtGetWishlistID.executeQuery();
+            wishlistIDResultSet.next();
+            int wishlistID = wishlistIDResultSet.getInt(1);
+
+
+            String insertNewWish = "INSERT INTO wish (name, description, link, price, picture, wishlist_id) VALUES (?, ? ,? ,? ,? ,?)";
+            PreparedStatement pstmt = connection.prepareStatement(insertNewWish);
+            pstmt.setString(1, newWish.getName());
+            pstmt.setString(2, newWish.getDescription());
+            pstmt.setString(3, newWish.getLink());
+            pstmt.setDouble(4, newWish.getPrice());
+            pstmt.setString(5, newWish.getPicture());
+            pstmt.setInt(6, wishlistID);
+            int affectedRows = pstmt.executeUpdate();
+
+            isAdded = affectedRows > 0;
+        }catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+
+        return isAdded;
+    }
 }
