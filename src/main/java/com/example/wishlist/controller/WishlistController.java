f5a3f0fe8ac8af7c41dadb8f2cdac80c82f53905
chye0001 <chye0001@stud.kea.dk>
2024-04-10 10:13:00 +0200

    added /addWish and /{wishlistName}/addWish endpoints

--- src/main/java/com/example/wishlist/controller/WishlistController.java
+++ src/main/java/com/example/wishlist/controller/WishlistController.java
@@ -1,11 +1,12 @@
 package com.example.wishlist.controller;
 
+import com.example.wishlist.model.Wish;
 import com.example.wishlist.service.WishlistService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/wishlist")
@@ -13,18 +14,31 @@ public class WishlistController {
 
     private WishlistService wishlistService;
 
-    public WishlistController(WishlistService wishlistService){
+    public WishlistController(WishlistService wishlistService) {
         this.wishlistService = wishlistService;
     }
 
     @GetMapping("/create")
-    public String createWishlist(){
+    public String createWishlist() {
         return "wishlist-create";
     }
 
     @PostMapping("/create")
-    public String createWishlist(@RequestParam("title") String wishlistTitle, @RequestParam String pictureLink){
+    public String createWishlist(@RequestParam("title") String wishlistTitle, @RequestParam String pictureLink) {
         wishlistService.createWishlist(wishlistTitle, pictureLink);
+        return "redirect:/wishlist";
+    }
+
+    @GetMapping("/{wishlistName}/addWish")
+    public String showPageForAddingWish(Model model, @PathVariable String wishlistName) {
+        List<Wish> wishlist = wishlistService.getWishes(wishlistName);
+        model.addAttribute("wishes", wishlist);
+        return "addWish";
+    }
+
+    @PostMapping("/addWish")
+    public String addWishToWishlist() {
+
         return "redirect:/wishlist";
     }
 }
