5364262cab75ca61ab6c66735fcc7235bc6fa1b2
Lars <laha0006@stud.kea.dk>
2024-04-10 23:53:28 +0200

    implemented endpoints for login and register pages.

--- src/main/java/com/example/wishlist/controller/UserController.java
+++ src/main/java/com/example/wishlist/controller/UserController.java
@@ -1,4 +1,20 @@
 package com.example.wishlist.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+
 }
