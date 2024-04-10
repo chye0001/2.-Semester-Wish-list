53e6759b495c0ab859464e0e62fe5c4f98b3c19e
chri26eq <chri26eq@stud.kea.dk>
2024-04-10 12:42:58 +0200

    added List<Wishlist> getAllWishlists ();

--- src/main/java/com/example/wishlist/repository/CRUDOperations.java
+++ src/main/java/com/example/wishlist/repository/CRUDOperations.java
@@ -1,6 +1,7 @@
 package com.example.wishlist.repository;
 
 import com.example.wishlist.model.Wish;
+import com.example.wishlist.model.Wishlist;
 
 import java.util.List;
 
@@ -9,6 +10,8 @@ public interface CRUDOperations {
     boolean createWishlist(String wishlistTitle, String pictureLink);
 
     List<Wish> getWishes (String wishlistName);
+
+    List<Wishlist> getAllWishlists ();
 
     boolean addWish(Wish newWish, String wishlistName);
 }
