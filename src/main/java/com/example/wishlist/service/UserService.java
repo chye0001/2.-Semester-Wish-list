1e43c1d3ae9b6deabffa84dc71816f838b021de2
Lars <laha0006@stud.kea.dk>
2024-04-11 01:14:20 +0200

    added passwordEnconder dependency to userService, and UserService now encodes the password.

--- src/main/java/com/example/wishlist/service/UserService.java
+++ src/main/java/com/example/wishlist/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.wishlist.service;
 import com.example.wishlist.dto.UserDto;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
 
@@ -10,15 +11,17 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private UserDetailsManager userDetailsManager;
+    private PasswordEncoder passwordEncoder;
 
-    public UserService(UserDetailsManager userDetailsManager) {
+    public UserService(UserDetailsManager userDetailsManager, PasswordEncoder passwordEncoder) {
         this.userDetailsManager = userDetailsManager;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void registerUser(UserDto user) {
         UserDetails userToRegister = User.builder()
                 .username(user.username())
-                .password(user.password())
+                .password(passwordEncoder.encode(user.password()))
                 .roles("USER").build();
         userDetailsManager.createUser(userToRegister);
     }
