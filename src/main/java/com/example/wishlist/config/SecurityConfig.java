7d22bdeb1807af876ab454f6f6ec47c1cddca8b9
Lars <laha0006@stud.kea.dk>
2024-04-11 01:10:19 +0200

    added passwordEnconder Bean with BCrypt enconder.

--- src/main/java/com/example/wishlist/config/SecurityConfig.java
+++ src/main/java/com/example/wishlist/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -37,6 +39,11 @@ public class SecurityConfig {
     @Bean
     public UserDetailsManager users(DataSource dataSource) {
         return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 
