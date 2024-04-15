c54b5b67f1896bbbe5e4cf41f7f3268126c81a7b
chye0001 <chye0001@stud.kea.dk>
2024-04-15 12:32:18 +0200

    added comment to addWishToWishlist()

--- src/main/java/com/example/wishlist/controller/WishlistController.java
+++ src/main/java/com/example/wishlist/controller/WishlistController.java
@@ -50,7 +50,7 @@ public class WishlistController {
         return "addWish";
     }
 
-    @PostMapping("{wishlistId}/addwish")
+    @PostMapping("{wishlistId}/addwish") //wishlistId bliver automatisk på wishlistId attributten i Wish-klassen, da det hedder det samme.
     public String addWishToWishlist(@ModelAttribute Wish newWish) {
         wishlistService.addWish(newWish);
 
