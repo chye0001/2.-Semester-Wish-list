7e949d339c0c4d85f7c8ace6d5f2394515243d8f
chye0001 <chye0001@stud.kea.dk>
2024-04-16 14:21:54 +0200

    added imports and annotations

---
+++ src/test/java/com/example/wishlist/controller/ShareControllerTest.java
@@ -0,0 +1,34 @@
+package com.example.wishlist.controller;
+
+import com.example.wishlist.model.Wish;
+import com.example.wishlist.model.Wishlist;
+import com.example.wishlist.service.WishlistService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(ShareController.class)
+class ShareControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private WishlistService wishlistService;
+
+    @Test
+    void viewSharedWishlist() throws Exception {
+        mockMvc.perform(get("/wishlist/1/share"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("/wishlist/viewSharedWishlist"));
+    }
+}
