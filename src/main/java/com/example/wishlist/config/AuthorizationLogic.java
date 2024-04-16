148856e3c1cbe910e3e7baf8ca27a7eb2abd6ad6
Lars <laha0006@stud.kea.dk>
2024-04-16 13:56:31 +0200

    refactor

--- src/main/java/com/example/wishlist/config/AuthorizationLogic.java
+++ src/main/java/com/example/wishlist/config/AuthorizationLogic.java
@@ -1,19 +1,20 @@
 package com.example.wishlist.config;
 
 import com.example.wishlist.repository.JdbcWishlistRepository;
+import com.example.wishlist.repository.WishlistRepository;
 import org.springframework.stereotype.Component;
 
 @Component("authz")
 public class AuthorizationLogic {
 
-    private JdbcWishlistRepository wishlistJDBC;
+    private WishlistRepository wishlistRepository;
 
-    public AuthorizationLogic(JdbcWishlistRepository wishlistJDBC) {
-        this.wishlistJDBC = wishlistJDBC;
+    public AuthorizationLogic(WishlistRepository wishlistRepository) {
+        this.wishlistRepository = wishlistRepository;
     }
 
 
     public boolean hasPermission(long id,String username) {
-        return wishlistJDBC.checkIdAndUsernameMatches(id,username);
+        return wishlistRepository.checkIdAndUsernameMatches(id,username);
     }
 }
