d9b471dcd38289bd7bf79e32bd3a0a9f691ecedc
Lars <laha0006@stud.kea.dk>
2024-04-16 13:39:02 +0200

    repository methods mostly implemented

--- src/main/java/com/example/wishlist/repository/WishRepository.java
+++ src/main/java/com/example/wishlist/repository/WishRepository.java
@@ -5,15 +5,9 @@ import com.example.wishlist.model.Wish;
 import java.util.List;
 
 public interface WishRepository {
-
     long addWish(Wish wish);
-
-    Wish getWish(long wishId);
-
-    List<Wish> getWishes(long wishId);
-
-    boolean deleteWish(long wishId);
-
-    boolean updateWish(Wish updatedWish);
-
+    Wish getWish(long id);
+    boolean deleteWish(long id);
+    boolean editWish(Wish wish);
+    boolean deleteSelectedWishes(List<Integer> wishIdList);
 }
