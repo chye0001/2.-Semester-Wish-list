1704b55888003017a569e5f65952f522b4013378
chye0001 <chye0001@stud.kea.dk>
2024-04-17 13:12:41 +0200

    added reserveWish() and WishService() to ShareController

--- src/main/java/com/example/wishlist/controller/ShareController.java
+++ src/main/java/com/example/wishlist/controller/ShareController.java
@@ -2,11 +2,13 @@ package com.example.wishlist.controller;
 
 import com.example.wishlist.model.Wish;
 import com.example.wishlist.model.Wishlist;
+import com.example.wishlist.service.WishService;
 import com.example.wishlist.service.WishlistService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -16,8 +18,10 @@ import java.util.List;
 public class ShareController {
 
     private final WishlistService wishlistService;
-    public ShareController (WishlistService wishlistService) {
+    private final WishService wishService;
+    public ShareController (WishlistService wishlistService, WishService wishService) {
         this.wishlistService = wishlistService;
+        this.wishService = wishService;
     }
 
     @GetMapping("")
@@ -36,5 +40,19 @@ public class ShareController {
         model.addAttribute("wishes", wishes);
 
         return "/wishlist/viewSharedWishlist";
+    }
+
+    @PostMapping("/wish/{wishId}/reserve")
+    public String reserveWish(Model model, @PathVariable long wishlistId, @PathVariable long wishId) {
+        wishService.reserveWish(wishId);
+        Wishlist wishlist = wishlistService.getWishlistById(wishlistId);
+
+        String wishlistName = wishlist.getName();
+        List<Wish> wishes = wishlist.getWishes();
+
+        model.addAttribute("wishlistName", wishlistName);
+        model.addAttribute("wishes", wishes);
+
+        return "redirect:/wishlist/viewSharedWishlist";
     }
 }
