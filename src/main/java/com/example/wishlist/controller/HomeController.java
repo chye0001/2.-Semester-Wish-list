20f0a29fea7e9416a5ad2517789c78c7a3393f09
Lars <laha0006@stud.kea.dk>
2024-04-18 18:54:01 +0200

    homeController activeLink model

--- src/main/java/com/example/wishlist/controller/HomeController.java
+++ src/main/java/com/example/wishlist/controller/HomeController.java
@@ -1,13 +1,16 @@
 package com.example.wishlist.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("activeLink","home");
+
         return "home";
     }
 }
