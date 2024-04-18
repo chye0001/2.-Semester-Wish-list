b5805191dd42299cd88b48b2918da79e6c890d75
Lars <laha0006@stud.kea.dk>
2024-04-18 23:10:11 +0200

    changes

--- src/main/java/com/example/wishlist/controller/WishlistController.java
+++ src/main/java/com/example/wishlist/controller/WishlistController.java
@@ -4,6 +4,7 @@ import com.example.wishlist.dto.WishSelectedDto;
 import com.example.wishlist.dto.WishlistFormDto;
 import com.example.wishlist.model.Wishlist;
 import com.example.wishlist.service.WishlistService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,11 +23,13 @@ public class WishlistController {
     }
 
     @GetMapping("")
-    public String wishlistMainPage(Model model, Authentication authentication) {
+    public String wishlistMainPage(Model model, Authentication authentication, HttpServletRequest request) {
         model.addAttribute("activeLink","wishlist");
         String username = authentication.getName();
         List<Wishlist> wishlistList = wishlistService.getAllWishlists(username);
         model.addAttribute("wishlists", wishlistList);
+        String baseUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort();
+        model.addAttribute("baseUrl", baseUrl);
         return "wishlist/wishlistOverview";
     }
 
@@ -50,7 +53,7 @@ public class WishlistController {
     }
 
     @GetMapping("/{wishlistId}")
-    public String viewWishlistById(@PathVariable long wishlistId, Model model) {
+    public String viewWishlistById(@PathVariable long wishlistId, Model model, HttpServletRequest request) {
         model.addAttribute("activeLink","wishlist");
         Wishlist wishlist = wishlistService.getWishlistById(wishlistId);
         WishSelectedDto selectedWishes = new WishSelectedDto(List.of());
@@ -59,21 +62,28 @@ public class WishlistController {
         model.addAttribute("selectedWishes", selectedWishes);
         model.addAttribute("wishlistId", wishlist.getWishlistId());
         model.addAttribute("wishlist", wishlist);
-
+        String baseUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
         boolean isPublic = wishlist.isPublic();
 
         if (isPublic) {
-            String wishlistLink = "localhost:8080/wishlist/" + wishlistId + "/shared"; //bør nok ændres i fremtiden
+            String wishlistLink = baseUrl + "/wishlist/" + wishlistId + "/shared"; //bør nok ændres i fremtiden
             model.addAttribute("wishlistLink", wishlistLink);
         }
         return "wishlist/viewWishlist";
     }
 
-    @PostMapping("/{wishlistId}/share")
-    public String setIsPublicToTrueForWishlist(@PathVariable long wishlistId) {
+    @GetMapping("/{wishlistId}/share")
+    public String setIsPublicToTrueForWishlist(@PathVariable long wishlistId, HttpServletRequest request) {
         wishlistService.setWishlistToPublic(wishlistId);
+        String referer = request.getHeader("Referer");
+        return "redirect:" + referer;
+    }
 
-        return "redirect:/wishlist/" + wishlistId;
+    @GetMapping("/{wishlistId}/hide")
+    public String setIsPublicToFalseForWishlist(@PathVariable long wishlistId, HttpServletRequest request) {
+        wishlistService.setWishlistToPrivate(wishlistId);
+        String referer = request.getHeader("Referer");
+        return "redirect:" + referer;
     }
 
     @GetMapping("/{wishlistId}/delete")
